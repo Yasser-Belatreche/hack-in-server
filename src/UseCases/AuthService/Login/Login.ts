6775a44a9dcac39f6e7f6ff9120aede154671c84
yasserBelatreche <yasser.belatreche0@gmail.com>
2022-03-05 08:44:41 +0100

    fix login bug

--- src/UseCases/AuthService/Login/Login.ts
+++ src/UseCases/AuthService/Login/Login.ts
@@ -5,7 +5,9 @@ class LoginFactory {
   constructor(private readonly studentsGateway: IStudentsGateway) {}
 
   async login(email: string, password: string): Promise<string> {
-    const student = await this.studentsGateway.findByEmail(email);
+    const student = await this.studentsGateway.findByEmail(
+      email.toLowerCase().trim()
+    );
 
     if (!student) throw new Error("student not exsit");
     if (student.password !== password.trim()) throw new Error("wrong passwrod");
